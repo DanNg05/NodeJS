6be2d909a2bd952a893373372766b6ec8244199e
Dan Nguyen <sydan05071997@gmail.com>
2024-07-13 16:28:12 +0930

    stuck with req.body

--- app.js
+++ app.js
@@ -18,8 +18,15 @@ connectDB();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+
+//bootstrap, fontawesome
 app.use('/css', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/css')));
 app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts')));
+app.use("/css",express.static("./node_modules/bootstrap/dist/css"));
+app.use("/js",express.static("./node_modules/bootstrap/dist/js"));
+//or
+app.use("/",express.static("./node_modules/bootstrap/dist/"));
+
 
 
 // register view engine
